1ed671627a2f40c188586cd43f768ce2edb350d8
Demetrio <tereschenko.d@triadasite.ru>
2024-04-18 11:06:53 +0300

    Fix data format

--- src/Protocols/Pusher/Channels/Concerns/InteractsWithPresenceChannels.php
+++ src/Protocols/Pusher/Channels/Concerns/InteractsWithPresenceChannels.php
@@ -20,7 +20,7 @@ trait InteractsWithPresenceChannels
         parent::broadcastInternally(
             [
                 'event' => 'pusher_internal:member_added',
-                'data' => $data ? json_decode($data, associative: true, flags: JSON_THROW_ON_ERROR) : [],
+                'data' => $data,
                 'channel' => $this->name(),
             ],
             $connection
@@ -42,7 +42,7 @@ trait InteractsWithPresenceChannels
             parent::broadcast(
                 [
                     'event' => 'pusher_internal:member_removed',
-                    'data' => ['user_id' => $userId],
+                    'data' => json_encode(['user_id' => $userId]),
                     'channel' => $this->name(),
                 ],
                 $connection
